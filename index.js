598465dc197902f55774864d1b82f188bd07bc0a
Suhaimali VP <suhaimalivpsuhaim@gmail.com>
2025-06-23 15:02:09 +0530

    Serve static HTML files using Express

--- index.js
+++ index.js
@@ -1,17 +1,7 @@
 ﻿const express = require('express');
-const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 
-const pages = ['surahs', 'hifz', 'prayer-times', 'qibla', 'tasbih', 'tafseer', 'donate', 'logout'];
-pages.forEach(page => {
-  app.get(/, (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', \\.html\));
-  });
-});
-
-app.listen(port, () => {
-  console.log(\Server running on port \\);
-});
+app.listen(port, () => console.log(Server running on port ));
